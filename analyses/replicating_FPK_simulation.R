f32116acdf083130692f11e832fff06c0088928b
David Bapst <dwbapst@gmail.com>
2018-08-02 10:13:06 -0500

    more simplification, moving things out of trait-specific area of code

--- analyses/replicating_FPK_simulation.R
+++ analyses/replicating_FPK_simulation.R
@@ -1,6 +1,28 @@
 
 
+# actualistic bounds on function
+getTraitBounds<-function(x){
+	bounds <- c(
+		min(x)-((max(x) - min(x))/2),
+		max(x)+((max(x) - min(x))/2)
+		)
+	return(bounds)
+	}
 
+
+traitData<-rnorm(100,0,1)
+# need traits to calculate bounds
+bounds<-getTraitBounds(traitData)
+
+# example from vignette for 
+	# should make two peak landscape
+params<-c(
+	a=10,
+	b=0.5,
+	c=0,
+	sigma=1,
+	bounds)
+trait<-traitData[1]
 
 
 #' @rdname intrinsicModels
@@ -8,15 +30,13 @@
 landscapeFPK_Intrinsic <- function(params, states, timefrompresent) {
 	#a discrete time FPK
 
-		# actualistic bounds on function
-	getTraitBounds<-function(x){
-		bounds <- c(
-			min(x)-((max(x) - min(x))/2),
-			min(x)+((max(x) - min(x))/2)
-			)
-		return(bounds)
-		}
 
+# From Boucher et al:
+# Finally, note that both BM and the OU model are special cases of the FPK
+# model: BM corresponds to V(x)=0 and OU to
+# V(x)=((alpha/sigma^2)*x^2)-((2*alpha*theta/(sigma^2))*x)
+
+	########################################################
 	# equation for getting potential	
 	potentialFun<-function(x,a,b,c){
 		# V(x)=ax4+bx2+cx 
@@ -26,77 +46,26 @@ landscapeFPK_Intrinsic <- function(params, states, timefrompresent) {
 
 	############################################
 
-	# need traits to calculate bounds
-	bounds<-getTraitBounds(trait)
-
-
 	# parameters: a,b,c, rootState, sigma
-
-
-# lexical scoping example
-funA<-function(a,b,c){
-	z<-sum(a,b,c)
-	newFun<-function(x){
-		x*z
-		}
-	return(newFun)
-	}
-
-funB<-funA(a=1,b=2,c=3)
-funB(1)
-funB(2)
-funB<-funA(a=2,b=3,c=4)
-funB(1)
-funB(2)
-
-
-	
-	# example from vignette for 
-		# should make two peak landscape
-	params<-c(
-		a=10,
-		b=0.5,
-		c=0,
-		sigma=1,
-		,bounds)
-
-	a <- params[1]
-	b <- params[2]
-	c <- params[3]
+		# unnecc: rootState 
 	sigma <- params[4]
-	bounds <- params [5:6]
-
-
-#	rootState <- 
+	bounds <- params[5:6]
 
 	# sim controls
-	grainscaleFPK = 100
+	grainscaleFPK <- 10
 	
-	
-    sd <- params[1]
-    attractor <- params[2]
-	#in this model, this should be between zero and one
-    attraction <- params[3]    
-	
-	
-	
-	
-	
-	
-	
-	
-
+grainScale<-grainscaleFPK
+# rename M
+# rename eigM
 
 
 
 ##############################################################################
 
-# all of the following only need to be run when the parameters of FPK are changed
+# all of the following only need to be run
+	# when the parameters of FPK are changed
+# this can be pre-calculated for a single run with lexical scoping	
 	
-	# need a function that will return last values returned if all parameters match
-	# or will calculate new values if they don't match
-
-
 # landscape descriptor function
 # over the arbitrary interval (-1.5 : 1.5)
 seqInterval<-seq(from=-1.5,to=1.5,length.out=grainScale)
@@ -105,7 +74,7 @@ seqInterval<-seq(from=-1.5,to=1.5,length.out=grainScale)
 # V(x)=ax4+bx2+cx 
 potentialVector<-potentialFun(
 	x=seqInterval,
-	a=a,b=b,c=c)	
+	a=params[1],b=params[2],c=params[3])	
 #
 # Coefficient of Diffusion of the Model
 dCoeff <- log((sigma)^2/2) 
@@ -115,31 +84,36 @@ dCoeff <- log((sigma)^2/2)
 # returns: the transition matrix going forward in time, for simulating traits only
 #
 # make empty matrix
-M <- matrix(0,grainScale,grainScale)
+expD <- M <- matrix(0,grainScale,grainScale)
 #assign values not on outer rows/columns
-for (i in 2:(grainScale-1)){
-	M[i-1,i] <- exp((potentialVector[i]-potentialVector[i-1])/2)
-	M[i+1,i] <- exp((potentialVector[i]-potentialVector[i+1])/2)
-	M[i,i] <- -(M[i-1,i]+M[i+1,i])
+for (i in 1:(grainScale)){
+	if(i>1){
+		M[i-1,i] <- exp((potentialVector[i]-potentialVector[i-1])/2)
+		}
+	if(i<grainScale){
+		M[i+1,i] <- exp((potentialVector[i]-potentialVector[i+1])/2)
+		}
+	# rate of staying in place is negative sum of neighboring cells
+	neighbors<-c(ifelse(i>1,M[i-1,i],0),
+			ifelse(i<grainScale,M[i+1,i],0)
+			)
+	M[i,i] <- (-sum(neighbors))
 	}
-# assign values to transition matrix not assigned by for loop
-M[2,1] <- exp((potentialVector[1]-potentialVector[2])/2)
-M[1,1] <- -M[2,1]
-M[grainScale-1,grainScale] <- exp((potentialVector[grainScale]-potentialVector[grainScale-1])/2)
-M[grainScale,grainScale] <- -M[grainScale-1,grainScale]
-# eigen of transition matrix
-eig <- eigen(M)
-#
-passage <- matrix(NA,dim(eig$vectors)[1],dim(eig$vectors)[2])
-for (col in 1:dim(eig$vectors)[2]){
-	passage[,col] <- Re(eig$vectors[,col])
-	}  
+
+# eigenvalues and eigenvectors of transition matrix
+	# take only real components
+eigM <- lapply(eigen(M),Re)
 # 			
-# obtain the matrix diagonal
-tPassage <- solve(passage,tol = 1e-30) 
+# solve the eigenvectors
+solvedEigenvectors <- solve(eigM$vectors,tol = 1e-30) 
+
+
+# Convolution product over one branch
+# what is this tau? depends on bounds, grainScale
 tau <- (((bounds[2]-bounds[1])/(grainScale-1))^2)
-vDiag <- diag(Re(eig$values)),
-diag_expD <- exp(dCoeff)/tau*diag(vDiag) 
+# old time-dep version from Boucher et al's code
+#diag(expD) <- exp(t*diag_expD)
+diag(expD) <- exp(exp(dCoeff)/tau*eigM$values)
 
 
 ###############################################
@@ -148,11 +122,15 @@ diag_expD <- exp(dCoeff)/tau*diag(vDiag)
 
 ###############################################
 
-	x <- states
+	x <- trait
 	#t <- tree$edge.length[i]
 
 	# write to which point of the grid a given position belongs to, 'continuous' version
 	X <- rep(0,grainScale)  
+	
+	
+
+
 	if (x==bounds[2]){
 		X[grainScale] <- 1
 	}else{
@@ -162,6 +140,7 @@ diag_expD <- exp(dCoeff)/tau*diag(vDiag)
 		X[ix+2] <- ux
 		X[ix+1] <- 1-ux
 		}
+
 	X<-X*(grainScale-1)/(bounds[2]-bounds[1])	
 		
 	# get the probability density / likelihood of each future trait value
@@ -170,14 +149,14 @@ diag_expD <- exp(dCoeff)/tau*diag(vDiag)
 		
 	
 
-	# Convolution product over one branch
-	expD <- matrix(0,grainScale,grainScale)
-	# old time-dep vertsion
-	#diag(expD) <- exp(t*diag_expD)
-	diag(expD) <- exp(diag_expD)
-	a <- passage%*%expD%*%tPassage%*%X
+
+	proptemp2 <- passage%*%expD%*%solvedEigenvectors
+	proptemp2%*%X
+	proptemp <- passage%*%expD%*%solvedEigenvectors%*%X
+	proptemp
+
 	# prevent rounding errors for small numbers	
-	proptemp<-apply(a,1,function(x) max(x,0))
+	proptemp<-apply(proptemp,1,function(x) max(x,0))
 					
 	# sample from this probability distribution	to get a descendent node value
 	newdisplacement <- sample(
