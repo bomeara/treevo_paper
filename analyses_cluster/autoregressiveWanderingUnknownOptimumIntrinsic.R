d9510ef713aaf16cd6e706d8d38fe3bfa4e814b7
Brian O'Meara <omeara.brian@gmail.com>
2019-07-11 16:05:16 -0400

    for tweaking for htcondor

---
+++ analyses_cluster/autoregressiveWanderingUnknownOptimumIntrinsic.R
@@ -0,0 +1,35 @@
+#' @rdname intrinsicModels
+#' @export
+autoregressiveWanderingUnknownOptimumIntrinsic <- function(params, states, timefrompresent) {
+	# 3) Time - autoregressive model with optimum based on a
+		# factor that changes through time (like O2 concentration)
+	# Parameters of the regression: 
+		# a single variable function to convert O2 to optimal gill size or whatever
+		# strength of pull
+		# BM wiggle
+	
+	# 10-20-18
+	# can't do it with an unknown optimum because the model function doesn't talk to other
+	# instances of the model, so how would they know what the optimum is at a particular time-point
+	# ->>>>>>>>>>>>>>>> 
+	#       We need a dataset with an environmental dataset
+	#            that we can treat as an optimum being tracked
+	# but what? and this will require another argument.
+		
+
+
+
+    #a discrete time OU, same sd, mean, and attraction for all chars
+    #params[1] is sd (sigma), params[2] is attractor (ie. character mean), params[3] is attraction (ie. alpha)
+    sigma <- params[1]
+    attractor <- params[2]
+    attraction <- params[3]    #in this model, this should be between zero and one
+    #subtract current states because we want displacement
+	newdisplacement <- TreEvo:::rnormFastZig(
+		nZig = length(states), 
+		meanZig = (attractor-states)*attraction, 
+		sdZig = sigma) 
+	
+	
+    return(newdisplacement)
+} 
---
+++ analyses_cluster/simulations_setup_script.R
@@ -0,0 +1,52 @@
+###################################################
+# Individual Empirical Analyses and Simulations
+##################################################
+
+# Control Box
+
+# number of simulated trait datasets to do for simulated-trait runs
+nSimTrait <- 10
+
+# error for run with mis-specified prior on sigmasq in a pure-BM model
+	# the mean of the normal prior is multiplied by this value
+	# 100 = mean of rate prior is off by two orders of magnitude!
+ratePriorError <- 100
+
+# root age for idealized simulated trees from time=0
+	# similar to Anolis root depth (51.49056)
+idealTreeDepth <- 50    
+
+# simulation resolution
+	# recc default is 1000
+generation.time <- 200000 
+
+# control parameters for multicore and simulation resolution
+multicore <- TRUE 
+coreLimit <- 6
+
+# control parameters for MCMC / ABC
+nRuns <- 1            		  # use 2 - recc default is 2 
+								#(for testing, use 1)
+nStepsPRC <- 2        		  # use 5 - recc default is 5 
+								#(for testing, use 2)
+numParticles <- 5 			  # use 300 - recc default is 300 
+								#(for testing, use 5)
+nInitialSimsPerParam <- 10 	  # use 100 - recc default is 100 
+								#(for testing, use 10)
+nInitialSims <- 5			  # use NULL - default is NULL = 100 per param 
+								#(for testing, use 5)
+
+#### miscellaneous controls
+# save data during runs?
+saveData <- FALSE
+# print out progress to terminal?
+verboseParticles <- FALSE
+
+###########################
+# FOR CONTINUING FROM A PREVIOUS TEST
+# (...if output from previous analyses exist at all)
+continueFromPrevious <- FALSE
+
+source(
+	"simulations_framework_script.R"
+	)
